ea39c6f77e54ac2193c086734be1e8819d5e84a1
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2024-04-25 10:47:28 -0300

    Fixed TiempoField.jsx

--- src/components/Form/Modulos-Reutilizables/TiempoField.jsx
+++ src/components/Form/Modulos-Reutilizables/TiempoField.jsx
@@ -1,55 +1,37 @@
 import React from 'react';
-import { Form, InputNumber } from 'antd';
+import { Form, Select } from 'antd';
 
-const nombresMeses = {
-    1: 'un',
-    2: 'dos',
-    3: 'tres',
-    4: 'cuatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'siete',
-    8: 'ocho',
-    9: 'nueve',
-    10: 'diez',
-    11: 'once',
-    12: 'doce',
-};
+const { Option } = Select;
 
 const TiempoField = () => {
-    const validateTiempo = (_, value) => {
-        if (value < 1 || value > 12) {
-            return Promise.reject('El tiempo debe ser un número entre 1 y 12');
-        }
-        return Promise.resolve();
-    };
-
-    const transformTiempo = (value) => {
-        if (value && nombresMeses[value]) {
-            return `${nombresMeses[value]} (${value}) ${value === 1 ? 'mes' : 'meses'}`;
-        }
-        return value;
-    };
 
     return (
-        <Form.Item
-            className="form-item"
-            label="Tiempo de SPP"
-            name="tiempo"
-            rules={[
-                {
-                    required: true,
-                    message: 'El tiempo de la SPP es obligatorio',
-                },
-                {
-                    validator: validateTiempo,
-                },
-            ]}
-            getValueFromEvent={(value) => transformTiempo(value)}
-        >
-            <InputNumber min={1} max={12} placeholder="Ingrese el tiempo de la SPP en meses (1-12)" />
-        </Form.Item>
-    );
-};
+    <Form.Item
+        className="form-item"
+        label="Tiempo de SPP"
+        name="plazo"
+        rules={[
+            {
+                required: true,
+                message: 'El plazo de la SPP es obligatorio',
+            },
+        ]}
+    >
+        <Select placeholder="Seleccione el plazo de SPP">
+            <Option value="un (1) mes">1</Option>
+            <Option value="dos (2) meses">2 </Option>
+            <Option value="tres (3) meses">3</Option>
+            <Option value="cuatro (4) meses">4</Option>
+            <Option value="cinco (5) meses">5</Option>
+            <Option value="seis (6) meses">6</Option>
+            <Option value="siete (7) meses">7</Option>
+            <Option value="ocho (8) meses">8</Option>
+            <Option value="nueve (9) meses">9</Option>
+            <Option value="diez (10) meses">10</Option>
+            <Option value="once (11) meses">11</Option>
+            <Option value="doce (12) meses">12</Option>
+        </Select>
+    </Form.Item>)
+}
 
 export default TiempoField;
